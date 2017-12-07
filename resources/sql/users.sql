86c57035ff6afd180c19577f8eb0ba9e2bba6331
Isaac Khor <s.github@isaackhor.com>
2017-12-07 23:48:02 +0800

    Update users queries to use INSERT ... RETURNING

--- resources/sql/users.sql
+++ resources/sql/users.sql
@@ -1,16 +1,18 @@
--- :name create-user! :! :n
+-- :name create-user! :<! :1
 -- :doc creates a new user record
 INSERT INTO users
 (id, email, passhash, apikey)
-VALUES (:id, :email, :passhash, :apikey);
+VALUES (:id, :email, :passhash, :apikey)
+RETURNING *;
 
--- :name update-user! :! :n
+-- :name update-user! :<! :1
 -- :doc updates an existing user record
 UPDATE users SET
   email = :email,
   passhash = :passhash,
   apikey = :apikey
-WHERE id = :id;
+WHERE id = :id
+RETURNING *;
 
 -- :name get-user-by-id :? :1
 -- :doc retrieves a user record given the id
@@ -23,7 +25,13 @@ SELECT * FROM users
 WHERE email = :email;
 
 -- :name get-apikey-by-email :? :1
+-- :doc retrieves a user's apikey given the email
 SELECT apikey FROM users
+WHERE email = :email;
+
+-- :name get-passhash-by-email :? :1
+-- :doc retrieves a user's passhash given the email
+SELECT passhash FROM users
 WHERE email = :email;
 
 -- :name delete-user! :! :n

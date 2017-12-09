4c6fa6801c7c0a41d5b22975814626472416edaa
Isaac Khor <s.github@isaackhor.com>
2017-12-09 15:09:58 +0800

    Fix sql queries for tags

--- resources/sql/tags.sql
+++ resources/sql/tags.sql
@@ -1,38 +1,35 @@
 -- :name create-tag! :<! :1
 -- :doc create a new tag
 INSERT INTO tags
-(owner, userid, name)
-VALUES (:ownerid, :userid, :name)
+(userid, easyid, name)
+VALUES (:userid, :easyid, :name)
 RETURNING *;
 
 -- :name update-tag! :<! :1
 -- :doc updates an existing tag record
 UPDATE tags SET
-  owner = :ownerid,
-  userid = :userid,
   name = :name
-WHERE id = :id
+WHERE userid = :userid AND easyid = :easyid
 RETURNING *;
 
--- :name get-tag-by-userid :? :1
+-- :name get-tag :? :1
 -- :doc retrieve a tag given a userid and an ownerid
 SELECT * FROM tags
-WHERE owner = :ownerid
-AND userid = :userid;
+WHERE userid = :userid AND easyid = :easyid;
 
 -- :name get-all-tags-for-user :? :*
 -- :doc get all tags owned by owner
 SELECT * FROM tags
-WHERE owner = :ownerid;
+WHERE userid = :userid;
 
--- :name get-max-userid :? :1
--- :doc gets the highest userid for an owner
-SELECT max(userid) FROM tags
-WHERE owner = :ownerid;
+-- :name get-max-easyid :? :1
+-- :doc gets the highest tag easyid for an owner
+SELECT max(easyid) FROM tags
+WHERE userid = :userid;
 
 -- :name get-tags-for-notif :? :*
 -- :doc retrieve all tags for a notification
-SELECT tags.id, tags.name FROM tags
-INNER JOIN notif_tag_join
-ON tags.id = notif_tag_join.tagid
-WHERE notifid = :notifid;
+SELECT * FROM tags
+INNER JOIN notif_tags
+ON tags.userid = notif_tags.userid AND tags.easyid = notif_tags.tagid
+WHERE notif_tags.notifid = :notifid;

b3f927cf67c82095fbb4251ec9a3cfd0e80f2daf
Isaac Khor <s.github@isaackhor.com>
2017-12-10 18:47:45 +0800

    Add time to notif

--- src/cljs/hermes/views/notifs.cljs
+++ src/cljs/hermes/views/notifs.cljs
@@ -1,17 +1,26 @@
 (ns hermes.views.notifs
   (:require [hermes.views.common :as common]
             [cljs-react-material-ui.reagent :as ui]
-            [re-frame.core :as rf]))
+            [cljs-react-material-ui.core :as cui]
+            [re-frame.core :as rf]
+            [cljs-time.format :as fmt]))
+
+(def time-fmt (fmt/formatter "yyyy-MM-dd HH:MM:ss"))
 
 (defn notifs-elem
   [{:keys [id title content time tags]}]
   [ui/list-item
-   [:div title]])
+   [:div.notif__elem
+    [ui/mui-theme-provider
+     {:mui-theme (cui/get-mui-theme
+                   {:palette {:text-color (cui/color :grey600)}})}
+     [:span.notif__time (fmt/unparse time-fmt time)]]
+    ": "
+    [:span.notif__title title]]])
 
 (defn notifs-page
   "The main notifications page"
   []
-  (.log js/console (str @(rf/subscribe [:notifs.map])))
   [:div.notifs-page
    [common/appbar]
    [common/nav-drawer]
--- src/cljs/hermes/views/tags.cljs
+++ src/cljs/hermes/views/tags.cljs
@@ -1,1 +1,7 @@
 (ns hermes.views.tags)
+
+(defn tags-page
+  "Tags page"
+  []
+  (let [a "b"]
+    [:div "tags"]))

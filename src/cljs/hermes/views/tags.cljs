84caf5dfe049641373ef0a550b5a6e49091cf95f
Isaac Khor <s.github@isaackhor.com>
2017-12-11 14:31:38 +0800

    Implement tag screen that doesn't fill entire page

--- src/cljs/hermes/views/tags.cljs
+++ src/cljs/hermes/views/tags.cljs
@@ -6,32 +6,35 @@
 
 (defn tag-elem [tag]
   [ui/table-row
-   [ui/table-row-column (ic/editor-mode-edit)]
    [ui/table-row-column (:id tag)]
    [ui/table-row-column
     {:style {:width "100%"}}
-    (:name tag)]])
+    (:name tag)]
+   [ui/table-row-column (ic/editor-mode-edit)]])
 
 (defn tags-page
   "Tags page"
   []
-  [:div.tags-page
+  [:div.tags-page.page-over
    [common/appbar]
    [common/nav-drawer]
-   [:div.tags-list
-    [ui/paper {:z-depth 1}
-     [ui/table {:selectable false
-                :multi-selectable false
-                :fixed-header false
-                :show-row-hover true
-                :style {:table-layout "auto"
-                        :width "auto"}}
-      [ui/table-header {:display-select-all false
-                        :adjust-for-checkbox false}
-       [ui/table-header-column "Edit"]
-       [ui/table-header-column "Tag ID"]
-       [ui/table-header-column "Tag Name"]]
-      [ui/table-body {:display-row-checkbox false}
-       (for [tag @(rf/subscribe [:tags.all])]
-         ^{:key (:id tag)}
-         [tag-elem tag])]]]]])
+   [:div.page
+    [ui/paper {:z-depth 1
+               :class-name "page-container"}
+     [:div.page-content
+      [:div.page__label "Tags"]
+      [ui/table {:selectable false
+                 :multi-selectable false
+                 :fixed-header false
+                 :show-row-hover true
+                 :style {:table-layout "auto"
+                         :width "auto"}}
+       [ui/table-header {:display-select-all false
+                         :adjust-for-checkbox false}
+        [ui/table-header-column "Tag ID"]
+        [ui/table-header-column "Tag Name"]
+        [ui/table-header-column "Edit"]]
+       [ui/table-body {:display-row-checkbox false}
+        (for [tag @(rf/subscribe [:tags.all])]
+          ^{:key (:id tag)}
+          [tag-elem tag])]]]]]])

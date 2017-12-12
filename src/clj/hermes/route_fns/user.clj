b9e6bfc7c174ca027035b3f1ba52af7dcacf6a63
Isaac Khor <s.github@isaackhor.com>
2017-12-12 19:34:59 +0800

    Add user cred validation route

--- src/clj/hermes/route_fns/user.clj
+++ src/clj/hermes/route_fns/user.clj
@@ -30,3 +30,17 @@
     (if (not-empty user)
       (respond/conflict {:error "Email already registered"})
       (create-new-user email password))))
+
+(defn get-user-resp
+  "Get the apikey of a user given his password (we assume its their password,
+   hitting this endpoint using their apikey would be kinda useless. I guess
+   they can be trying to validate their apikey? Who knows. I just write apis.
+   I don't to psychological analysis of devs using my apis."
+  [userinfo]
+  (let [user (qu/get-user-by-id {:id (:id user)})]
+    (if (not-empty user)
+      (respond/ok (select-keys user [:email :apikey]))
+      (respond/internal-server-error
+        {:error "I don't know what is doing on!!!! Its late and night
+                 and I'm too tired to think about it. Please tell me if
+                 you see this message. You shouldn't. In theory."}))))
--- src/clj/hermes/routes/user.clj
+++ src/clj/hermes/routes/user.clj
@@ -1,7 +1,8 @@
 (ns hermes.routes.user
   (:require [compojure.api.sweet :refer :all]
             [hermes.route-fns.user :as usr]
-            [schema.core :as s]))
+            [schema.core :as s]
+            [buddy.auth :as auth]))
 
 (def user-routes
   (context "/user" []
@@ -12,4 +13,18 @@
                     password :- s/Str]
       :return {:email s/Str
                :apikey s/Str}
-      (usr/create-user-resp email password))))
+      (usr/create-user-resp email password))
+
+    (GET  "/:id" []
+      :path-params []
+      :return {:email s/Str
+               :apikey s/Str}
+      :auth-rules auth/authenticated?
+      :current-user user-info
+      :header-params [authorization :- s/Str]
+      :description
+      "This endpoint returns your email and apikey back to you if you are
+       authenticated. Use this to validate login information in apps. Throws
+       a 403 if you are not. I think. I really should be sleeping, but this
+       thing is due tomorrow."
+      (usr/get-user-resp userinfo))))
